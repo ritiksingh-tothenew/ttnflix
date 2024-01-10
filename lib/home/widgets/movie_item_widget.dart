d8c6f4825574ee0c2b98cb2e2b219a8df36f3a58
ritik <ritik.singh@tothenew.com>
2024-01-10 13:23:50 +0530

    Completed Movie detail Page

--- lib/home/widgets/movie_item_widget.dart
+++ lib/home/widgets/movie_item_widget.dart
@@ -1,5 +1,7 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+import '../../navigation/ttnflix_autoroute.gr.dart';
 import '../../themes/ttnflix_sizes.dart';
 import '../model/movie_list.dart';
 
@@ -22,14 +24,16 @@ class MovieItemWidget extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Stack(
-          alignment: Alignment.bottomCenter,
-          children: [
-            bannerWidget,
-            Positioned.fill(
-              child: InkWell(
-                onTap: () {},
-                child: const Padding(
+        InkWell(
+          onTap: () {
+            context.router.push(MovieDetailScreenRoute(movie: movie));
+          },
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              bannerWidget,
+              const Positioned.fill(
+                child: Padding(
                   padding: EdgeInsets.all(TtnFlixSize.size16),
                   child: Align(
                     alignment: Alignment.topRight,
@@ -40,30 +44,30 @@ class MovieItemWidget extends StatelessWidget {
                   ),
                 ),
               ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                decoration:
-                    BoxDecoration(color: Colors.black.withOpacity(0.15)),
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                      left: TtnFlixSize.size16, right: TtnFlixSize.size16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        movie.getContentRating(),
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                      Text(movie.originalLanguage?.toUpperCase() ?? '',
-                          style: const TextStyle(color: Colors.white))
-                    ],
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  decoration:
+                      BoxDecoration(color: Colors.black.withOpacity(0.15)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: TtnFlixSize.size16, right: TtnFlixSize.size16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          movie.getContentRating(),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        Text(movie.originalLanguage?.toUpperCase() ?? '',
+                            style: const TextStyle(color: Colors.white))
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
         SizedBox(
           height: isGridView ? TtnFlixSize.size2 : TtnFlixSize.size16,
